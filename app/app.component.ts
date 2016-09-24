09c6528ce9643c39fad18a4295e1c0e64b7916b2
Maksymilian Sadowski <tmax1993@gmail.com>
2016-09-25 00:05:35 +0200

    Angular2 tutorial. Getting started with some standard stuff.

---
+++ app/app.component.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core';
+
+export class Character {
+  id: number;
+  name: string;
+}
+
+const CHARACTERS: Character[] = [
+  { id: 1, name: 'Shabbyabby' },
+  { id: 2, name: 'Shabika' },
+  { id: 3, name: 'Felietta' }
+];
+
+@Component({
+  selector: 'my-app',
+  styles: [`
+  	.characters {
+  		margin: 0 0 2em 0;
+  		list-style-type: none;
+  		padding: 0;
+  		width: 20em;
+  	}
+  	.characters li {
+  		color: #FFE300;
+  		border-radius: 5px;
+  		background-color: #004D98;
+  		height: 1.5em;
+  		padding: .4em;
+  		margin-bottom:  .4em;
+  		border: 2px #FFE300 solid;
+  	}
+  	.selected {
+  		background-color: #0084ff !important;
+  	}
+  	.characters li.selected:hover {
+	    background-color: #66b5ff !important;
+  	}
+  	.characters li:hover {
+	    background-color: #0069cc;
+	    left: .1em;
+    }
+  	.characters .text {
+    	position: relative;
+    	top: -3px;
+  	}
+  	.characters .badge {
+    	display: inline-block;
+    	font-size: small;
+    	color: #004D98;
+    	padding: 0.8em 0.7em 0 0.7em;
+    	background-color: #FFE600;
+    	line-height: 1em;
+    	position: relative;
+    	left: -6px;
+    	top: -6px;
+    	height: 2em;
+    	margin-right: .8em;
+    	border-radius: 4px 0 0 4px;
+  	}
+  `],
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Characters</h2>
+    <ul class="characters">
+    	<li *ngFor="let character of characters"
+    	[class.selected]="character === selectedCharacter"
+    	(click)="onSelect(character)">
+    		<span class="badge">{{character.id}}</span> {{character.name}}
+    	</li>
+    </ul>
+    <div *ngIf="selectedCharacter">
+    	<h2>{{selectedCharacter.name}} details!</h2>
+    	<div>
+    		<label>id: </label>{{selectedCharacter.id}}
+    	</div>
+    	<div>
+      		<label>name: </label>
+      		<input [(ngModel)]="selectedCharacter.name" placeholder="name">
+    	</div>
+    </div>
+    `
+})
+
+export class AppComponent {
+  title = 'AltTracker';
+  characters = CHARACTERS;
+  selectedCharacter: Character;
+
+  onSelect(character: Character): void {
+  	this.selectedCharacter = character;
+  }
+}
